925e454dc28f03e86ceb7adb04f41f6da609c575
Micah Woods <micahwoods@gmail.com>
2011-10-12 01:39:04 -0500

    Added 'display' and 'remove' to plugin

--- jquery.notice.js
+++ jquery.notice.js
@@ -1,34 +1,49 @@
 (function($) {
 
-	var defaults = {
-		note: function(message) {},
-		success: function(message) {},
-		notice: function(message) {},
-		error: function(message) {},
-		warning: function(message) {}
-	};
 
-	var methods = {
-		init: function(options) {
-			if (options) $.extend(defaults, options);
-		},
-		note: function(message) {},
-		success: function(message) {},
-		notice: function(message) {},
-		error: function(message) {},
-		warning: function(message) {}
-	};
+  var defaults = {
+    note    : null,
+success : null,
+notice  : null,
+error   : null,
+warning : null
+  };
 
-	$.fn.notice = function(method) {
-		// Method calling logic
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === 'object' || ! method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error('Method ' + method + ' does not exist on jQuery.tooltip');
-		}
-	};
+  var methods = {
+    init: function(options) {
+            if (options) $.extend(defaults, options);
+          },
+
+display: function(klass, message) {
+           return $(this).each(function () {
+             $(this).append(getHtmlString(klass, message));
+           });
+
+         },
+
+remove: function(klass, message) {
+          return $(this).each(function () {
+            $("." + klass + ":contains(" + message + ")").remove();
+          });
+        }
+  };
+
+  var getHtmlString = function (klass, message) {
+    return (defaults[klass] !== null && typeof defaults[klass] === "function") ?
+      defaults[klass]({message: message}) :
+      "<p class='" + klass + "'>" + message + "</p>"
+  };
+
+  $.fn.notice = function(method) {
+    // Method calling logic
+    if (methods[method] && typeof methods[method] === "function") {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if (typeof method === 'object' || ! method) {
+      return methods.init.apply(this, arguments);
+    } else {
+      $.error('Method ' + method + ' does not exist on jQuery.tooltip');
+    }
+  };
 
 })(jQuery);
 
