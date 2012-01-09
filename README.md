2d05f307fa5396f648d5c70fe284ee60108d8382
Micah Woods <micahwoods@gmail.com>
2012-01-08 21:48:30 -0600

    Updated readme and no longer return element.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Most of the code I write is for government contracts so very little makes it into the wild.  I wrote this code for a 'lunch-n-learn' at Praeses. I am proud of it because although it is relatively small, it demonstrates several Javascript strategies including private global variables and the module pattern using immediate functions. Some of it was borrowed from various places, some css from Ryan Bates nifty-generators and the some from the jQuery plugin guides.
+
+This was a simple demonstration for employees new to Javascript and jQuery.  This Plugin could use html5 `data` attributes to store state and remove elements in a much more sophisticated way.
+
 # jquery.notice
 
 This plugin was written for demonstration purposes.  Use at your own demise, muhahahahahaha!!!
--- jquery.notice.js
+++ jquery.notice.js
@@ -1,5 +1,6 @@
 (function($) {
 
+  // Default values, override these with functions, or strings.
 	var defaults = {
 		note: null,
 		success: null,
@@ -8,17 +9,17 @@
 		warning: null
 	};
 
+  // methods that can be executed
 	var methods = {
 		init: function(options) {
 			if (options) $.extend(defaults, options);
 		},
 
 		display: function(klass, message) {
-			var $element = $(_getHtmlString(klass, message));
-			$(this).each(function() {
-				$(this).append($element);
+			var element = _getHtmlString(klass, message);
+			return $(this).each(function() {
+				$(this).append(element);
 			});
-			return $element;
 		},
 
 		remove: function(klass, message) {
@@ -28,10 +29,12 @@
 		}
 	};
 
+  // Error Message Logging
 	var _error = function(method) {
 		return $.error('Method ' + method + ' does not exist on jquery.notice');
 	}
 
+  // Decides if the method is a string or a function and returns the html or error
 	var _getHtmlString = function(klass, message) {
 		if (defaults[klass] !== null && typeof defaults[klass] === "function") {
 			return defaults[klass]({
@@ -47,6 +50,7 @@
 		return _error(klass);
 	};
 
+  // Standard jQuery, from the jQuery guides
 	$.fn.notice = function(method) {
 		// Method calling logic
 		if (methods[method] && typeof methods[method] === "function") {
